b061ae7b051d671b4f7acb3903244eaa27381a5a
matiiss-z <matiissdz@gmail.com>
2019-03-16 11:44:55 +0200

    routes

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = require('./routes/api/posts');
+const posts = require('./routes/api/posts/');
 
-app.use('/api/posts', posts);
+app.use('/api/posts/', posts);
 
 // Handle Production
 if(process.env.NODE_ENV === 'production') {
